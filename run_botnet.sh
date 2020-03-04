4417953b0e90c3f693a7d79239f6d0fd4ff249b0
jzhou316 <jzhou02@g.harvard.edu>
2020-03-04 10:23:02 -0500

    bug fix for bash function

--- run_botnet.sh
+++ run_botnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-train {
+train () {
   gpu=$1
   topo=$2
 
@@ -11,6 +11,13 @@ train {
   --data_name "$topo" \
   --batch_size 2 \
   --enc_sizes 32 32 32 32 32 32 32 32 32 32 32 32 \
+  --act relu \
+  --residual_hop 1 \
+  --deg_norm rw \
+  --final proj \
+  --epochs 50 \
+  --lr 0.005 \
+  --early_stop 1 \
   --save_dir ./saved_models \
   --save_name "$topo"_model_lay12_rh1_rw_ep50.pt
 }
